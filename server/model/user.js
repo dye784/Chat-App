66401d414234711a45912a53fba30c4d1dcd8708
dye784 <damon.ye91@gmail.com>
2017-05-03 23:10:02 -0400

    feat: add hooks beforeCreate and beforeUpdate to model, changed setEmailAndPassword function to const and moved above model

--- server/model/user.js
+++ server/model/user.js
@@ -2,6 +2,14 @@ const { STRING, VIRTUAL } = require('sequelize');
 const bcrypt = require('bcryptjs');
 const db = require('./_db');
 
+const setEmailAndPassword = (user) => {
+  user.email = user.email && user.email.toLowerCase();
+  if (!user.password) { return Promise.resolve(user); }
+
+  return bcrypt.hash(user.get('password'), 10)
+    .then(hash => user.set('password_digest', hash));
+};
+
 const User = db.define('users', {
   name: STRING,
   email: {
@@ -13,15 +21,11 @@ const User = db.define('users', {
   },
   password_digest: STRING,
   password: VIRTUAL,
+}, {
+  hooks: {
+    beforeCreate: setEmailAndPassword,
+    beforeUpdate: setEmailAndPassword,
+  },
 });
-
-function setEmailAndPassword(user) {
-  user.email = user.email && user.email.toLowerCase();
-  if (!user.password) { return Promise.resolve(user); }
-
-  return bcrypt.hash(user.get('password'), 10)
-    .then(hash => user.set('password_digest', hash));
-}
-
 
 module.exports = User;
