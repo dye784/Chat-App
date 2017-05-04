5037e84b8a82d5a30a797a9abd86ae95c00458ce
dye784 <damon.ye91@gmail.com>
2017-05-04 08:48:57 -0400

    feat: add before and afterEach for user auth test

--- tests/server/model/user.test.js
+++ tests/server/model/user.test.js
@@ -1,18 +1,18 @@
-/* global describe it before aftereach */
+/* global describe it before afterEach */
 
-const User = require('../../../server/model/user');
+const db = require('../../../server/model/');
+const { User } = db;
 const { expect } = require('chai');
 
 describe('User Model', () => {
+  before('Clear database', () => User.sync({ force: true }));
+  afterEach('Clear database', () => User.sync({ force: true }));
+
   describe('instanceMethods', () => {
     describe('authenticate', () => {
-      it('returns true when stored hashed password matches password after hashing', () => {
-        // body...;
-      });
+      it('returns true when stored hashed password matches password after hashing', () => {});
 
-      it('returns false when stored hashed passwords do not match', () => {
-        // body...;
-      });
+      it('returns false when stored hashed passwords do not match', () => {});
     });
   });
 });
