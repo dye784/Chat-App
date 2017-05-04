92702444d3b32c52c6b551a0adba2810364188dd
dye784 <damon.ye91@gmail.com>
2017-05-04 11:46:09 -0400

    feat: set up local auth strategy, post request to local auth

--- server/app/routes/auth.js
+++ server/app/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const User = require('../../model/user');
 
 passport.serializeUser((user, done) => {
@@ -16,9 +17,33 @@ passport.deserializeUser((id, done) => {
   .catch(done);
 });
 
-// Get self
+// Set up Auth Local strategy
+passport.use(new LocalStrategy((email, password, done) => {
+  User.findOne({
+    where: { email },
+  })
+  .then((foundUser) => {
+    if (!foundUser) {
+      return done(null, false, { message: 'Login incorrect' });
+    }
+
+    return foundUser.authenticate(password)
+      .then((isAuthorized) => {
+        if (!isAuthorized) {
+          return done(null, false, { message: 'Login incorrect' });
+        }
+        done(null, foundUser);
+      });
+  })
+  .catch(done);
+}));
+
+// GET request to get self
 router.get('/', (req, res, next) => {
   res.send(req.user);
 });
+
+// POST request for Local Login
+router.post('/login', passport.authenticate('local', { successRedirect: '/' }));
 
 module.exports = router;
