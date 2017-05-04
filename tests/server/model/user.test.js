6b28d45b096c930a9ee80476fd78d6afdea1e776
dye784 <damon.ye91@gmail.com>
2017-05-04 09:28:35 -0400

    feat: add mocha, chai, test script to package.json, fix user model unqiue value schema, auth test passing properly

--- tests/server/model/user.test.js
+++ tests/server/model/user.test.js
@@ -1,16 +1,24 @@
 /* global describe it before afterEach */
 
-const db = require('../../../server/model/');
-const { User } = db;
 const { expect } = require('chai');
+const User = require('../../../server/model/user');
 
 describe('User Model', () => {
   before('Clear database', () => User.sync({ force: true }));
   afterEach('Clear database', () => User.sync({ force: true }));
+  const userData = {
+    name: 'Example',
+    email: 'example@example.com',
+    password: '12345',
+  };
 
   describe('instanceMethods', () => {
     describe('authenticate', () => {
-      it('returns true when stored hashed password matches password after hashing', () => {});
+      it('returns true when stored hashed password matches password after hashing', () => {
+        return User.create(userData)
+          .then(createdUser => createdUser.authenticate('12345'))
+          .then(result => expect(result).to.be.true);
+      });
 
       it('returns false when stored hashed passwords do not match', () => {});
     });
