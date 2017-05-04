f6f35ef39837049b91822910fa76aca1437c28fc
dye784 <damon.ye91@gmail.com>
2017-05-04 17:40:16 -0400

    feat: add routes and tests for chatroom messages, add conditional server starting in main for testing, modify message model, adjust tests hooks

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const authRouter = require('./auth');
+const chatroomRouter = require('./chatroom');
 
 router.use('/auth', authRouter);
+router.use('/chatrooms', chatroomRouter);
 
 module.exports = router;
