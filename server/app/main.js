10d9216c2785d04ed5757dbe29e28105aa429264
dye784 <damon.ye91@gmail.com>
2017-05-03 22:03:09 -0400

    feat: add passport and session, setup boilerplate, add comments to middleware

--- server/app/main.js
+++ server/app/main.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
 const routes = require('./routes');
 const db = require('../model');
 
@@ -9,17 +11,36 @@ const isProduction = process.env.NODE_ENV === 'production';
 const port = isProduction ? process.env.PORT : 1337;
 const app = express();
 
+// Logging Middleware
 app.use(morgan('dev'));
+
+// Server up static files from '../../public'
 app.use(express.static(path.join(__dirname, '..', '..', 'public')));
+
+// Body parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Session Middleware
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'invalid secret key',
+  resave: false,
+  saveUninitialized: false,
+}));
+
+// Authentication Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Server up index.html file
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', '..', 'public', 'index.html'));
 });
 
+// Reroute to /api
 app.use('/api', routes);
 
+// Sync database then start listening
 db.sync()
 .then(() => {
   console.log('Database is Synced!');
