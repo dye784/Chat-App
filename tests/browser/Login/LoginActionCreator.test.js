69a09006831f1a4f52a9f432b995c6479c39a8d8
dye784 <damon.ye91@gmail.com>
2017-05-05 12:18:19 -0400

    chore: change describe block

--- tests/browser/Login/LoginActionCreator.test.js
+++ tests/browser/Login/LoginActionCreator.test.js
@@ -1,20 +1,24 @@
-/* global describe it before afterEach */
+/* global describe it beforeEach afterEach */
 
 const { expect } = require('chai');
 const { AUTHENTICATED, authenticate } = require('../../../browser/Login/LoginActionCreator');
 
 describe('Login Action Creators', () => {
-  it('returns an object with type AUTHENTICATED and a user', () => {
-    const exampleUser = {
-      email: 'example@exmaple.com',
-      password: 'examplePassword',
-    };
+  describe('Synchronous Action Creators', () => {
+    describe('authenticate', () => {
+      it('returns an object with type AUTHENTICATED and a user', () => {
+        const exampleUser = {
+          email: 'example@exmaple.com',
+          password: 'examplePassword',
+        };
 
-    const result = {
-      type: AUTHENTICATED,
-      user: exampleUser,
-    };
+        const result = {
+          type: AUTHENTICATED,
+          user: exampleUser,
+        };
 
-    expect(authenticate(exampleUser)).to.deep.equal(result);
+        expect(authenticate(exampleUser)).to.deep.equal(result);
+      });
+    });
   });
 });
