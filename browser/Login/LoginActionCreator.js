96011c0ae134d0827e719a518b510fbc4806b787
dye784 <damon.ye91@gmail.com>
2017-05-05 11:30:22 -0400

    feat: add login thunk

--- browser/Login/LoginActionCreator.js
+++ browser/Login/LoginActionCreator.js
@@ -1,6 +1,17 @@
+import axios from 'axios';
+
 export const AUTHENTICATED = 'AUTHENTICATED';
 
 export const authenticate = (user) => ({
   type: AUTHENTICATED,
   user,
 });
+
+export const login = (email, password) => (dispatch) => {
+  axios.post('/api/auth/login', {
+    email,
+    password,
+  })
+  .then(res => res.data)
+  .then(user => dispatch(authenticate(user)));
+};
