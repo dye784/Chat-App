0c7e557ab1fef2968f725ca708389d9bcc405376
dye784 <damon.ye91@gmail.com>
2017-05-06 11:40:31 -0400

    feat: add chat action creators sync and async

---
+++ browser/Chat/ChatActionCreators.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+export const ADD_NEW_MESSAGE = 'ADD_NEW_MESSAGE';
+export const LOAD_CHAT_MESSAGES = 'LOAD_CHAT_MESSAGES';
+
+export const addNewMessage = (message) => ({
+  type: ADD_NEW_MESSAGE,
+  message,
+});
+
+export const loadChatMessages = (messages) => ({
+  type: LOAD_CHAT_MESSAGES,
+  messages,
+});
+
+export const fetchAllMessagesForChatroom = (chatroomId) => (dispatch) => {
+  axios.get(`/api/chatrooms/${chatroomId}/messages`)
+  .then(res => res.data)
+  .then(receivedMessages => dispatch(loadChatMessages(receivedMessages)));
+};
+
+export const postNewMessageToServer = (chatroomId, message) => (dispatch) => {
+  axios.post(`/api/chatrooms/${chatroomId}/messages`, { message })
+  .then(res => res.data)
+  .then(createdMessage => dispatch(addNewMessage(createdMessage)))
+  .then(() => socket.emit('addMessage', { message }));
+};
