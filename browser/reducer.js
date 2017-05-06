efa8f91df2ffb8291887c2b4e762f570b358dbae
dye784 <damon.ye91@gmail.com>
2017-05-06 11:41:23 -0400

    feat: connected chat reducer

--- browser/reducer.js
+++ browser/reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import AppReducer from './App/AppReducer';
 import LoginReducer from './Login/LoginReducer';
+import ChatReducer from './Chat/ChatReducer';
 
 const rootReducer = combineReducers({
   auth: LoginReducer,
   app: AppReducer,
+  messages: ChatReducer,
 });
 
 export default rootReducer;
