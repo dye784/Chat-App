91cda8188753632bf55caa51b4bd0db3d14f3aff
dye784 <damon.ye91@gmail.com>
2017-05-06 12:31:00 -0400

    feat: wire up chat into app and add text input area

--- browser/App/index.js
+++ browser/App/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Login from '../Login/index.jsx';
 import Logout from '../Logout/index.jsx';
+import Chat from '../Chat/index.jsx';
 
 export const App = ({ user }) => {
   return (
     <div style={{ backgroundColor: 'blue', height: '200px' }}>
       {user ? <Logout /> : <Login /> }
+      <Chat />
     </div>
   );
 };
--- browser/Chat/index.jsx
+++ browser/Chat/index.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { postNewMessageToServer } from './ChatActionCreators';
 
-export const Chat = ({ messages }) => {
-  const messagesToDisplay = messages.map(message => <h1 id={`${message.id}`}>{message}</h1>);
-
-  return (
-    <div>
-      {messagesToDisplay}
-    </div>
-  );
-};
+export class Chat extends Component {
+  render() {
+    return (
+      <div style={{ backgroundColor: 'red', height: '200px' }}>
+        {this.props.messages.map(message => <h1 id={`${message.id}`}>{message}</h1>)}
+        <form onSubmit={this.onSubmit}>
+          <textarea name="message" rows="10"></textarea>
+          <button type="submit"></button>
+        </form>
+      </div>
+    )
+  }
+}
 
 const mapStateToProps = ({ messages }) => ({ messages });
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = { postNewMessageToServer };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Chat);
 
