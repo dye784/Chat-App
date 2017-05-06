3ded3236d2cec49ca86032b40a8d5c1607d878d6
dye784 <damon.ye91@gmail.com>
2017-05-06 11:56:19 -0400

    fix: add catch to thunks to console.error meaningful error

--- browser/Chat/ChatActionCreators.js
+++ browser/Chat/ChatActionCreators.js
@@ -16,12 +16,14 @@ export const loadChatMessages = (messages) => ({
 export const fetchAllMessagesForChatroom = (chatroomId) => (dispatch) => {
   axios.get(`/api/chatrooms/${chatroomId}/messages`)
   .then(res => res.data)
-  .then(receivedMessages => dispatch(loadChatMessages(receivedMessages)));
+  .then(receivedMessages => dispatch(loadChatMessages(receivedMessages)))
+  .catch(err => console.error('Fetching messages for chatroom unsuccessful', err));
 };
 
 export const postNewMessageToServer = (chatroomId, message) => (dispatch) => {
   axios.post(`/api/chatrooms/${chatroomId}/messages`, { message })
   .then(res => res.data)
   .then(createdMessage => dispatch(addNewMessage(createdMessage)))
-  .then(() => socket.emit('addMessage', { message }));
+  .then(() => socket.emit('addMessage', { message }))
+  .catch(err => console.error('Creating message unsuccessful', err));
 };
--- browser/Login/LoginActionCreator.js
+++ browser/Login/LoginActionCreator.js
@@ -13,16 +13,19 @@ export const login = (email, password) => (dispatch) => {
     password,
   })
   .then(res => res.data)
-  .then(user => dispatch(authenticate(user)));
+  .then(user => dispatch(authenticate(user)))
+  .catch(err => console.error('Login unsuccessful', err));
 };
 
 export const logout = () => (dispatch) => {
   axios.delete('/api/auth/logout')
-  .then(() => dispatch(authenticate(null)));
+  .then(() => dispatch(authenticate(null)))
+  .catch(err => console.error('Logout unsuccessful', err));
 };
 
 export const fetchLoggedInUser = () => (dispatch) => {
   axios.get('/api/auth/me')
   .then(res => res.data)
-  .then(user => dispatch(authenticate(user)));
+  .then(user => dispatch(authenticate(user)))
+  .catch(err => console.error('Fetching logged in user unsuccessful', err));
 };
