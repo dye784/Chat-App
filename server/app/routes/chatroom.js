ee5fa9a7350931f919a8fee000082ce7f0e6ab4a
dye784 <damon.ye91@gmail.com>
2017-05-08 00:11:45 -0400

    feat: add route for getting new messages

--- server/app/routes/chatroom.js
+++ server/app/routes/chatroom.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const Chatroom = require('../../model/chatroom');
 const Message = require('../../model/message');
+const User = require('../../model/user');
 
 // GET request to get all chatrooms
 router.get('/', (req, res, next) => {
@@ -20,6 +21,28 @@ router.get('/:chatroomId/messages', (req, res, next) => {
     order: [
       ['created_at', 'ASC'],
     ],
+  })
+  .then((foundMessages) => {
+    res.send(foundMessages);
+  })
+  .catch(next);
+});
+
+// Get new messages from chatroom since user logged in
+router.get('/:chatroomId/messages/new', (req, res, next) => {
+  User.findById(req.user.id)
+  .then((foundUser) => {
+    return Message.findAll({
+      where: {
+        chatroom_id: req.params.chatroomId,
+        created_at: {
+          $gte: foundUser.last_login,
+        },
+      },
+      order: [
+        ['created_at', 'ASC'],
+      ],
+    });
   })
   .then((foundMessages) => {
     res.send(foundMessages);
