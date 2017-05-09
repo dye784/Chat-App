73ae6ebc0551422b19c37500b1395f2959097877
dye784 <damon.ye91@gmail.com>
2017-05-09 16:31:06 -0400

    refactor: login into dumb component

--- browser/Login/index.jsx
+++ browser/Login/index.jsx
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { login } from './LoginActionCreator';
 
-export class Login extends Component {
-  onSubmit = (evt) => {
+export const Login = ({ login }) => {
+  const handleSubmit = (evt) => {
     evt.preventDefault();
-    this.props.login(evt.target.username.value, evt.target.password.value);
-  }
+    login(evt.target.username.value, evt.target.password.value);
+  };
 
-  render() {
-    return (
-      <div className="container-login">
-        <form className="login-form" onSubmit={this.onSubmit}>
-          <input className="box" name="username" placeholder="username" />
-          <input className="box"name="password" type="password" placeholder="password" />
-          <input className="box" type="submit" value="Login" />
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="container-login">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <input className="box" name="username" placeholder="username" />
+        <input className="box"name="password" type="password" placeholder="password" />
+        <input className="box" type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
 
 const mapDispatchToProps = { login };
 
