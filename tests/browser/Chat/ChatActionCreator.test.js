193a64e7d2a47dec157d1b14f2795073d555304c
dye784 <damon.ye91@gmail.com>
2017-05-08 21:09:42 -0400

    feat: add chat action creator tests

---
+++ tests/browser/Chat/ChatActionCreator.test.js
@@ -0,0 +1,28 @@
+/* global describe it */
+
+const { expect } = require('chai');
+const { ADD_NEW_MESSAGE, LOAD_CHAT_MESSAGES, addNewMessage, loadChatMessages } = require('../../../browser/Chat/ChatActionCreators');
+
+describe.only('Chat Action Creators', () => {
+  describe('addNewMessage', () => {
+    it('returns the correct action', () => {
+      const solution = {
+        type: ADD_NEW_MESSAGE,
+        message: 'Hello world!',
+      };
+
+      expect(addNewMessage('Hello world!')).to.deep.equal(solution);
+    });
+  });
+
+  describe('loadChatMessages', () => {
+    it('returns the correct action', () => {
+      const solution = {
+        type: LOAD_CHAT_MESSAGES,
+        messages: ['test', 1, 2, 3],
+      };
+
+      expect(loadChatMessages(['test', 1, 2, 3])).to.deep.equal(solution);
+    });
+  });
+});
