58aaec951580b1437751f06cb635ba51b4394080
dye784 <damon.ye91@gmail.com>
2017-06-23 15:55:06 -0400

    feat: add redis

--- server/app/main.js
+++ server/app/main.js
@@ -1,10 +1,17 @@
+// Express App Setup
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const session = require('express-session');
 const passport = require('passport');
 const app = express();
+
+// Redis and Sessions setup
+const redis = require('redis');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const redisClient = redis.createClient();
+const sessionStore = new RedisStore({ client: redisClient });
 
 const routes = require('./routes');
 const db = require('../model');
@@ -32,10 +39,14 @@ app.use(bodyParser.json());
 
 // Session Middleware
 app.use(session({
+  store: sessionStore,
+  key: 'jsessionid',
   secret: process.env.SESSION_SECRET || 'invalid secret key',
   resave: false,
   saveUninitialized: false,
 }));
+
+console.log(sessionStore);
 
 // Authentication Middleware
 app.use(passport.initialize());
